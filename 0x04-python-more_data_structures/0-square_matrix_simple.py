ec83bbf583b3ca30ce490936fdacd19e65f17bc0
themathematcs <chriznganga69@gmail.com>
2022-12-10 08:39:09 -0800

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-def square_matrix_simple(matrix=[]):
-    i = 0;
-    while i < 4:
-        newmatrix = matrix[i * 2]
-        i + 1
-    return newmatrix;
+def square_matrix_simple(matrix[]):
+     if matrix:
+           new = []
+           for rows in matrix:
+                 new.append([n ** 2 for n in rows])
+            return new
