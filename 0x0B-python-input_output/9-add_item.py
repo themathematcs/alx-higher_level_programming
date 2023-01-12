d503df5e64fb768125fd2f0b690e931799602f08
christian ng'ang'a <111011919+themathematcs@users.noreply.github.com>
2023-01-12 03:20:33 +0300

    Update 9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
 """
-    script that adds all arguments to a Python list,
-    and then save them to a file:
-    You must use your function save_to_json_file from
-    7-save_to_json_file.py
-    You must use your function load_from_json_file
-    from 8-load_from_json_file.py
-    The list must be saved as a JSON representation
-    in a file named add_item.json
-    If the file doesn’t exist, it should be created
-    You don’t need to manage file permissions / exceptions.
+    Module for class Student.
 """
 
 
-import json
-import sys
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+class Student:
+    """
+        A class students that defines a student by:
+        Attributes:
+            first_name (str): name of student.
+            last_name (str): name of student.
+            age (int): age of student.
+        Methods:
+            __init__ - initializes the Student instance.
+            to_json - retrieves dictionary repr of Student instance.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            Initialises Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-arg = sys.argv[1:]
-file_name = "add_item.json"
-
-try:
-    python_object = load_from_json_file(file_name)
-except FileNotFoundError:
-    save_to_json_file([], file_name)
-
-python_object = load_from_json_file(file_name)
-if type(python_object) is list:
-    for item in arg:
-        python_object.append(item)
-
-save_to_json_file(python_object, file_name)
+    def to_json(self):
+        """
+            retrieves a dictionary representation of Student.
+        """
+        return self.__dict__
