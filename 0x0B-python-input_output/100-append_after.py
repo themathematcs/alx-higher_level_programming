25b4da192312db3c70df2f1446368b0c00db703e
christian ng'ang'a <111011919+themathematcs@users.noreply.github.com>
2023-01-12 03:23:06 +0300

    Update 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """
-    function that inserts a line of text to a file,
-    after each line containing a specific string (see example):
-    Prototype:
-    def append_after(filename="", search_string="", new_string=""):
-    You must use the with statement
-    You don’t need to manage file permission or file doesn't exist
-    exceptions.
-    You are not allowed to import any module
+Module for append_after method.
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """ append a string into a new_line after find another """
-    text = ""
-    with open(filename, mode="r", encoding='utf-8') as f:
+    '''Method for inserting text after search string.'''
+    lines = []
+    with open(filename, "r", encoding="utf-8") as f:
         lines = f.readlines()
-    for line in lines:
-        if line.find(search_string) != -1:
-            text += line + new_string
-        else:
-            text += line
-    with open(filename, mode="w", encoding='utf-8') as f:
-        f.write(text)
+        i = 0
+        while i < len(lines):
+            if search_string in lines[i]:
+                lines[i:i + 1] = [lines[i], new_string]
+                i += 1
+            i += 1
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(lines)
