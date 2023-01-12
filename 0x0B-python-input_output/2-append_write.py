5016d91735e48b135659604735ad3d0093556611
christian ng'ang'a <111011919+themathematcs@users.noreply.github.com>
2023-01-12 03:10:41 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
 """
-    function that reads n lines of a text file (UTF8)
-    and prints it to stdout:
-    Prototype: def read_lines(filename="", nb_lines=0):
-    Read the entire file if nb_lines is lower or equal
-    to 0 OR greater or equal to the total number of lines
-    of the file
-    You must use the with statement
-    You don’t need to manage file permission or file doesn't
-    exist exceptions.
+Module for append_write method.
 """
 
 
-def read_lines(filename="", nb_lines=0):
-    """ read nb_lines of a text """
-    with open(filename, encoding='utf-8') as f:
-        if (nb_lines <= 0):
-                print(f.read(), end="")
-        else:
-            cont = 0
-            while (cont < nb_lines):
-                print(f.readline(), end="")
-                cont += 1
+def append_write(filename="", text=""):
+    """append_write appends string at the end of a text file.
+    Args:
+        filename (str): name of file.
+            text (str): text to be appended to the file.
+        Returns: number of characters written.
+    """
+    with open(filename, "a", encoding='utf-8') as a_file:
+        return a_file.write(text)
