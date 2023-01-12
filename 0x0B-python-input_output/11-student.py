08223460a7ff2cf03825fe27da13b2fa1193b10b
christian ng'ang'a <111011919+themathematcs@users.noreply.github.com>
2023-01-12 03:26:10 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""
-    class Student that defines a student by:
-    Public instance attributes:
-        first_name
-        last_name
-        age
-    Instantiation with first_name, last_name and age:
-    def __init__(self, first_name, last_name, age):
-    Public method def to_json(self): that retrieves a
-    dictionary representation of a Student instance
-    (same as 10-class_to_json.py)
-    You are not allowed to import any module
-"""
+'''Module for Student class.'''
 
 
 class Student:
-    """ class to create a dict obj """
-
+    '''Class for jsonification.'''
     def __init__(self, first_name, last_name, age):
-        """ initialization for Student object """
-        self.age = age
-        self.last_name = last_name
+        '''Constructor.'''
         self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    def to_json(self):
-        """ retrieves a dictionary representation """
-        return self.__dict__
+    def to_json(self, attrs=None):
+        '''Retrieves dictionary with filter.'''
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        '''Loads attributes from json.'''
+        for key, value in json.items():
+            self.__dict__[key] = value
